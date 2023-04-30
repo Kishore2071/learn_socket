87be34b5f77ebd6db617f6c79703057855422792
Kishore kumar <kishorekumarsaran2001@gmail.com>
2023-04-30 18:41:38 +0530

    Tried basic multithreading chat server

--- basic.py
+++ basic.py
@@ -1,21 +1,22 @@
 import socket
 
-HOST= ''
-PORT= 3074
+HOST = ''
+PORT = 3074
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-                    # AF - Address Family
-                    # INET - Internet
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                # AF - Address Family
+                # INET - Internet
 
-s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
-s.bind((HOST,PORT))
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
 s.listen()
-while(True):
-    conn,addr=s.accept()
-    #conn - the connection object with the client
+while (True):
+    conn, addr = s.accept()
+    # conn - the connection object with the client
     # addr - array 0 - IP address of the client
-    #        array 1 - the back port with which the connection is established
-    print("IP {} Connected".format(addr[0]))
-    conn.sendall(b"Hello,Welcome to this workshop")
+    #		array 1 - The back port with which the connection is established
+    print("IP {} connected".format(addr[0]))
+    print(addr)
+    conn.sendall(b"Hello, welcome to this workshop")
     conn.close()
 s.close()
---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+
+HOST = 'localhost'
+PORT = 3074
+
+name = input("Enter your name: ");
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF - Address Family
+# INET - Internet
+
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.connect((HOST, PORT))
+
+while (True):
+    msg = input("Message: ")
+    if (msg == "quit"):
+        break
+    msg = name + ": " + msg
+    s.send(msg.encode())
+s.close()
