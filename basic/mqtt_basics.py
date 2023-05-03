6a7dbcbe1887dcd71515ba996f04c34bfd94969f
Kishore kumar <kishorekumarsaran2001@gmail.com>
2023-05-03 18:15:54 +0530

    Move to seperate folder

---
+++ basic/mqtt_basics.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as mqtt
+
+HOST = 'broker.hivemq.com'
+PORT = 1883
+TIMEOUT = 60
+
+
+def on_message(client, userdata, message):
+    print("{}: {}".format(message.topic, str(message.payload)))
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected.")
+    client.subscribe('lahtp')
+
+
+client = mqtt.Client()
+client.on_message = on_message
+client.on_connect = on_connect
+
+client.connect(HOST, PORT, TIMEOUT)
+client.loop_forever()
