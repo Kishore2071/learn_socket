29dcc731af78d93711881d4a745697aa1d65d1f1
Kishore2071 <kishorekumarsaran2001@gmail.com>
2023-12-13 17:52:18 +0530

    Rough HTTP Server

--- basic.py
+++ basic.py
@@ -1,22 +1,52 @@
+#!/usr/bin/env python3
+
 import socket
+from threading import Thread
 
 HOST = ''
 PORT = 3074
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    # AF - Address Family
-                    # INET - Internet
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-s.bind((HOST,PORT))
-s.listen()
 
-while(True):
+def start_chat_thread(conn, addr):
+    t = ChatMsgThread(conn, addr)
+    t.start()
+
+
+class ChatMsgThread(Thread):
+    def __init__(self, conn, addr):
+        Thread.__init__(self)
+        self.conn = conn
+        self.addr = addr
+        print("IP {} connected".format(addr[0]))
+
+    def run(self):
+        self.conn.sendall(b"Send your message, I will be able to see it here.\n")
+        while (True):
+            try:
+                data = self.conn.recv(2048)
+                if not data:
+                    self.conn.close()
+                    continue
+                else:
+                    try:
+                        data = data.decode()
+                        print(data)
+                    except:
+                        pass
+            except:
+                pass
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind((HOST, PORT))
+s.listen()
+while (True):
     conn, addr = s.accept()
-        # conn - the connection object with the client
-        # addr - array 0 - IP address of the client
-        #		 array 1 - The back port with which the connection is established
-    print("IP {} connected".format(addr[0]))
-    print(addr)
-    conn.sendall(b"Hello, welcome to this workshop")
-    conn.close()
+    # conn - the connection object with the client
+    # addr - array 0 - IP address of the client
+    #		array 1 - The back port with which the connection is established
+    start_chat_thread(conn, addr)
 s.close()
+
+
--- client.py
+++ client.py
@@ -1,20 +1,25 @@
+#!/usr/bin/env python3
+
 import socket
 
 HOST = ''
 PORT = 3074
 
-name = input("Enter your name: ")
+name = input("Enter your name: ");
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                    # AF - Address Family
-                    # INET - Internet
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
-s.connect((HOST,PORT))
+# AF - Address Family
+# INET - Internet
 
-while(True):
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.connect((HOST, PORT))
+print(s.recv(2048).decode())
+while (True):
     msg = input("Message: ")
-    if(msg == "quit"):
+    if (msg == "quit"):
         break
     msg = name + ": " + msg
-    s.send(msg.encode())
+    s.sendall(msg.encode())
 s.close()
+
+
