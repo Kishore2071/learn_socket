b80ccc8da9dda2f477ff5767ece980e8da795fcd
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-03-13 16:32:27 +0530

    Implement Security Fix

--- Cyber Tools/mac changer/mac_changer.py
+++ Cyber Tools/mac changer/mac_changer.py
@@ -5,7 +5,10 @@ new_mac = input("Enter New mac: ")
 
 print("Changing Mac Address "+ interface + " To " + new_mac )
 
-subprocess.call("ifconfig " + interface + " down",shell=True )
-subprocess.call("ifconfig " + interface + " hw ether" + new_mac,shell=True )
-subprocess.call("ifconfig " + interface + " up",shell=True )
+# subprocess.call("ifconfig " + interface + " down",shell=True )
+# subprocess.call("ifconfig " + interface + " hw ether" + new_mac,shell=True )
+# subprocess.call("ifconfig " + interface + " up",shell=True )
 
+subprocess.call(["ifconfig " , interface , " down"] )
+subprocess.call(["ifconfig " , interface , " hw " + " ether" + new_mac] )
+subprocess.call(["ifconfig " , interface , " up"] )
