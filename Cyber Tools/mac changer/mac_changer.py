a389dc3c81d25c7b792576fa2599dade9351497f
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-03-13 18:07:52 +0530

    implement python argument

--- Cyber Tools/mac changer/mac_changer.py
+++ Cyber Tools/mac changer/mac_changer.py
@@ -1,14 +1,22 @@
 import subprocess
+import optparse
 
-interface = input("Enter Interfaces: ")
-new_mac = input("Enter New mac: ")
+# python mac_changer.py --interface eth0 --mac 00:44:44:55:77:33
+
+parser = optparse.OptionParser()
+
+parser.add_option("-i","--interface",dest="interface",help="Used to select interface")
+parser.add_option("-m","--mac",dest="new_mac",help="Used to select mac")
+
+(options,arguments) = parser.parse_args()
+
+interface = options.interface
+
+new_mac = options.new_mac
+
 
 print("Changing Mac Address "+ interface + " To " + new_mac )
 
-# subprocess.call("ifconfig " + interface + " down",shell=True )
-# subprocess.call("ifconfig " + interface + " hw ether" + new_mac,shell=True )
-# subprocess.call("ifconfig " + interface + " up",shell=True )
-
-subprocess.call(["ifconfig " , interface , " down"] )
-subprocess.call(["ifconfig " , interface , " hw " + " ether" + new_mac] )
-subprocess.call(["ifconfig " , interface , " up"] )
+subprocess.call(["ifconfig " , interface , " down"])
+subprocess.call(["ifconfig " , interface , " hw " + " ether" + new_mac])
+subprocess.call(["ifconfig " , interface , " up"])
