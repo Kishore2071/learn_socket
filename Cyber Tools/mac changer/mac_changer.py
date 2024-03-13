b9587843a1f546753a18b2f2acf40f5948060eb6
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-03-13 18:22:53 +0530

    Change program to function

--- Cyber Tools/mac changer/mac_changer.py
+++ Cyber Tools/mac changer/mac_changer.py
@@ -3,20 +3,17 @@ import optparse
 
 # python mac_changer.py --interface eth0 --mac 00:44:44:55:77:33
 
-parser = optparse.OptionParser()
+def get_options():
+    parser = optparse.OptionParser()
+    parser.add_option("-i","--interface",dest="interface",help="Used to select interface")
+    parser.add_option("-m","--mac",dest="new_mac",help="Used to select mac")
+    return parser.parse_args()
 
-parser.add_option("-i","--interface",dest="interface",help="Used to select interface")
-parser.add_option("-m","--mac",dest="new_mac",help="Used to select mac")
+def change_mac(interface,new_mac):
+    print("Changing Mac Address "+ interface + " To " + new_mac )
+    subprocess.call(["ifconfig " , interface , " down"])
+    subprocess.call(["ifconfig " , interface , " hw " + " ether" + new_mac])
+    subprocess.call(["ifconfig " , interface , " up"])
 
-(options,arguments) = parser.parse_args()
-
-interface = options.interface
-
-new_mac = options.new_mac
-
-
-print("Changing Mac Address "+ interface + " To " + new_mac )
-
-subprocess.call(["ifconfig " , interface , " down"])
-subprocess.call(["ifconfig " , interface , " hw " + " ether" + new_mac])
-subprocess.call(["ifconfig " , interface , " up"])
+(options,arguments) = get_options()    
+change_mac(options.interface,options.new_mac)
